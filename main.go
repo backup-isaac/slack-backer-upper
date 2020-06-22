75c7d856633cc7df75b7348376ed18762b930584
Isaac Weintraub <iweintraub3@gatech.edu>
2020-06-21 22:35:14 -0400

    use flag for flag parsing

--- main.go
+++ main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"slack-backer-upper/files"
 )
 
+var (
+	dirname = flag.String("d", "", "a directory to import")
+	zipname = flag.String("z", "", "a zip file to import")
+)
+
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatalln("usage: go run main.go <slack archive> [-z]")
-	}
-	if len(os.Args) > 2 {
-		if os.Args[2] == "-z" {
-			if err := files.Unzip(os.Args[1], "_archive"); err != nil {
-				log.Fatalf("Error unzipping file: %v\n", err)
-			}
-			if err := files.ImportFolder("_archive"); err != nil {
-				log.Printf("Error importing backup: %v\n", err)
-			}
-			if err := os.RemoveAll("_archive"); err != nil {
-				log.Fatalf("Error removing temp dir: %v\n", err)
-			}
-		} else {
-			log.Fatalln("usage: go run main.go <slack archive> [-z]")
+	flag.Parse()
+	if *zipname != "" {
+		if err := files.Unzip(*zipname, "_archive"); err != nil {
+			log.Fatalf("Error unzipping file: %v\n", err)
 		}
-	} else if err := files.ImportFolder(os.Args[1]); err != nil {
-		log.Printf("Error importing backup: %v\n", err)
+		if err := files.ImportFolder("_archive"); err != nil {
+			log.Printf("Error importing backup: %v\n", err)
+		}
+		if err := os.RemoveAll("_archive"); err != nil {
+			log.Fatalf("Error removing temp dir: %v\n", err)
+		}
+	} else if *dirname != "" {
+		if err := files.ImportFolder(*dirname); err != nil {
+			log.Printf("Error importing backup: %v\n", err)
+		}
+	} else {
+		log.Printf("Starting HTTP server...")
 	}
 }
