dc11fe4bdc88b8df9a4576f13a2f30d0b41fad7a
Isaac Weintraub <iweintraub3@gatech.edu>
2020-06-24 01:25:31 -0400

    input validation for loading messages, remove unnecessary time zone selector

--- server/static/archive-viewer.js
+++ server/static/archive-viewer.js
@@ -2,6 +2,7 @@ function populateChannels() {
   fetch("/channels").then((response) => {
     if (!response.ok) {
       document.getElementById("error").style.display = "block";
+      document.getElementById("select-params").style.display = "none";
       throw new Error(`GET /channels failed: ${response.status} ${response.statusText}`);
     }
     return response.json();
@@ -13,4 +14,37 @@ function populateChannels() {
       document.getElementById("channel").appendChild(option);
     }
   });
+}
+
+let selectedChannel = "";
+let selectedFrom = new Date(0);
+let selectedTo = new Date(0);
+
+function tryLoadMessages() {
+  const channel = document.getElementById("channel").value;
+  if (!channel) {
+    return;
+  }
+  const fromStr = document.getElementById("from").value;
+  if (!fromStr) {
+    return;
+  }
+  const toStr = document.getElementById("to").value;
+  if (!toStr) {
+    return;
+  }
+  const fromDay = fromStr.split("-");
+  const from = new Date(fromDay[0], fromDay[1] - 1, fromDay[2]);
+  const toDay = toStr.split("-");
+  const to = new Date(toDay[0], toDay[1] - 1, toDay[2]);
+  if (from.getTime() >= to.getTime()
+    || (channel === selectedChannel
+      && from.getTime() === selectedFrom.getTime()
+      && to.getTime() === selectedTo.getTime())) {
+    return;
+  }
+  selectedChannel = channel;
+  selectedFrom = from;
+  selectedTo = to;
+  console.log(`load messages in ${channel} from ${from} to ${to}`);
 }
