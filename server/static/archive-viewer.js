bfe7bfb572aac51762ecbbb04ea34cd60c8f5c2f
Isaac Weintraub <iweintraub3@gatech.edu>
2020-07-03 13:28:13 -0400

    view messages

--- server/static/archive-viewer.js
+++ server/static/archive-viewer.js
@@ -21,16 +21,56 @@ function loadMessages(channel, from, to) {
     if (!response.ok) {
       document.getElementById("error").style.display = "block";
       document.getElementById("select-params").style.display = "none";
-      console.error(`GET /messages failed: ${response.status} ${response.statusText}`);
-      return response.text();
+      throw new Error(`GET /messages failed: ${response.status} ${response.statusText}`);
     }
     return response.json();
   }).then((messages) => {
+    document.getElementById("error").style.display = "none";
     document.getElementById("select-params").style.display = "none";
     for (let message of messages) {
-      let text = document.createElement("p");
-      text.innerText = JSON.stringify(message);
-      document.getElementById("messages").appendChild(text);
+      let msgContainer = document.createElement("div");
+      let msgTime = document.createElement("span");
+      const time = new Date(message.timestamp * 1000);
+      msgTime.innerText = time.getFullYear().toString().padStart(4, "0") + "-"
+        + (time.getMonth() + 1).toString().padStart(2, "0") + "-"
+        + time.getDate().toString().padStart(2, "0") + " "
+        + time.getHours().toString().padStart(2, "0") + ":"
+        + time.getMinutes().toString().padStart(2, "0");
+      msgContainer.appendChild(msgTime);
+      let msgUser = document.createElement("strong");
+      msgUser.style.marginLeft = "20px";
+      msgUser.innerText = message.user;
+      msgContainer.appendChild(msgUser);
+      let msgBody = document.createElement("p");
+      msgBody.innerText = message.text;
+      msgContainer.appendChild(msgBody);
+      if (message.attachments) {
+        for (let attachment of message.attachments) {
+          let div = document.createElement("div");
+          let attach;
+          if (attachment.from_url) {
+            attach = document.createElement("a");
+            attach.innerText = attachment.title || attachment.from_url;
+            attach.href = attachment.from_url;
+          } else {
+            attach = document.createElement("p");
+            attach.innerText = attachment.fallback;
+          }
+          div.appendChild(attach);
+          div.style.marginLeft = "40px";
+          msgContainer.appendChild(div);
+        }
+      }
+      if (message.reacts) {
+        for (let name of Object.keys(message.reacts)) {
+          let reacc = document.createElement("span");
+          reacc.innerText = `:${name}: (${message.reacts[name].length})`
+          reacc.style.marginRight = "20px";
+          msgContainer.appendChild(reacc);
+        }
+      }
+      msgContainer.style.marginBottom = "20px";
+      document.getElementById("messages").appendChild(msgContainer);
     }
   });
 }
